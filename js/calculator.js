607b2b263491bb164a210b53dd2f52ab1933f43d
Owen Mount <96270460+fletchly@users.noreply.github.com>
2024-03-10 21:53:33 -0700

    Initial commit

---
+++ js/calculator.js
@@ -0,0 +1,101 @@
+init()
+const operations = ['a', 's', 'm', 'd'];
+let reset = true;
+let exp = "";
+let result = 0;
+const inputs = ['1','2','3','4','5','6','7','8','9','0','(',')','.'];
+const commands = new Map([
+    ['/', 'd'],
+    ['*', 'm'],
+    ['-', 's'],
+    ['+', 'a'],
+    ['=', 'e'],
+    ['Enter', 'e'],
+    ['Delete', 'ac']
+]);
+
+
+/**
+ * Initialize all calculator buttons with event listeners.
+ */
+function init() {
+    let buttons = document.getElementsByClassName("calc-button");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons.item(i).addEventListener("click", function () {
+            getInputs(this.id)
+        });
+    }
+
+    document.addEventListener('keydown', function (event) {
+        let key = event.key;
+        if (commands.has(key))
+        {
+            getInputs(commands.get(key));
+        } else if (inputs.includes(key))
+        {
+            getInputs(key);
+        }
+    });
+}
+
+/**
+ * Process inputs from keypad.
+ * @param val The value to add to the current register.
+ */
+function getInputs(val) {
+    let display = document.getElementById("disp"); // Select the display box
+
+    if (operations.includes(val)) // If the value passed in is one of the basic operators, flip reset to false, allowing the current register to be modified
+    {
+        reset = false;
+    }
+
+    switch (val)
+    {
+        case 'd': // Division
+            display.innerHTML += " &div; ";
+            exp += " / ";
+            break;
+        case 'm': // Multiplication
+            display.innerHTML += " &times; ";
+            exp += " * ";
+            break;
+        case 's': // Subtraction
+            display.innerHTML += " - ";
+            exp += " - ";
+            break;
+        case 'a': // Addition
+            display.innerHTML += " + ";
+            exp += " + ";
+            break;
+        case 'e': // Equals
+            result = eval(exp);
+            display.innerHTML = result;
+            exp = result
+            reset = true;
+            break;
+        case 'ac': // Clear all
+            clear(display);
+            exp = "";
+            break;
+        default:
+            if (reset) // If reset is enabled, clear everything before modifying register.
+            {
+                exp = "";
+                display.innerHTML = "";
+                reset = false;
+            }
+            display.innerHTML += val; // Display the value.
+            exp += val
+            break;
+    }
+}
+
+/**
+ * Clears given display.
+ * @param display The display to be cleared.
+ */
+function clear(display) {
+    display.innerHTML = "0";
+    reset = true;
+}
